46ea2806822e3b64753b0d1ab3be435664fe0693
Ayeb Mohamed <ayebmohamed09@gmail.com>
2023-12-09 01:30:19 +0000

    MINDWELL-HUB

--- src/components/Navbar/dropmen.js
+++ src/components/Navbar/dropmen.js
@@ -25,7 +25,7 @@ function Dropmen() {
         </li>
         <li>
           <a
-            href={`${window.location.href}/Podcasts`}
+            href="/podcasts"
             target="_blank"
             rel="noreferrer"
             className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-blue-700 dark:hover:text-white"
